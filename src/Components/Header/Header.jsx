1a78c47dd858a6d5f245508763264355f12cf179
Vladyslav Skakun <vskakun275l@gmail.com>
2025-03-29 16:14:13 +0100

    first

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react'
+
+import styles from './Header.module.css'
+
+import EarthIcon from '../../icons/EarthIcon.jsx'
+import Button from './Button/Button.jsx'
+
+const Header = ()=>{
+
+    const [language, setLanguage] = useState(localStorage.getItem('language') || 'en')
+
+    useEffect(() => {
+        localStorage.setItem('language', language)
+    }, [language])
+
+    const handleLanguage = (e) => {
+        
+        
+        const language = e.target.value
+        setLanguage(language)
+        
+    }
+
+    console.log(language);
+    
+
+
+    return(
+        <>
+        <header>
+            <div className={styles.leftBlock} >
+                <a href=""><img src="public\img\logoo.svg" alt="" /></a>
+            </div>
+            <div className={styles.rightBlock} >
+                <div className={styles.selectBlock} >
+                <EarthIcon/>
+                    <select name='language' value={language} onChange={handleLanguage}>
+                        <option value="en">{ language === 'en' ? 'English' : 'Англиский'}</option>
+                        <option value="ru">{ language === 'ru' ? 'Русский' : 'Russian'}</option>
+                    </select>
+                </div>
+                <a className={styles.Login} href="">{ language === 'ru' ? 'Войти' : 'Log in'}</a>  
+                <Button>{ language === 'ru' ? 'Регистрация' : 'Sign in'}</Button>
+
+                
+            </div>
+        </header>
+        
+        
+
+        </>
+    )
+}
+export default Header;
